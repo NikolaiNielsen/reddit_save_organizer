5602b39f09b902a39e06a20e524201c2d90bdf05
Nikolai Nielsen <nikolainielsen@outlook.dk>
2017-04-18 11:13:42 +0200

    simplify function args

--- bot.py
+++ bot.py
@@ -83,8 +83,8 @@ def get_new_saves(me,old,conn,cursor,lim = 25):
 		# if it's a submission we proceed
 		if postType == "submission":
 			# We check if the post is already in the DB
-			Id = i.id
-			if Id in old:
+			post_id = i.id
+			if post_id in old:
 				# print('This post is already in the DB! ' + Id)
 				continue
 			else:
@@ -95,17 +95,15 @@ def get_new_saves(me,old,conn,cursor,lim = 25):
 				# - link to the "link" (url)
 				# - number of comments (num_comments)
 				# - Subreddit (subreddit)
-				# print('It\'s a submission, id = ' + Id)
 				if i.is_self:
-					toDatabase.append((Id,i.title,i.shortlink,i.shortlink,
-						i.num_comments,i.subreddit.display_name))
+					toDatabase.append((post_id,i.title,i.shortlink,
+						i.shortlink,i.num_comments,i.subreddit.display_name))
 				else:
-					toDatabase.append((Id,i.title,i.shortlink,i.url,
+					toDatabase.append((post_id,i.title,i.shortlink,i.url,
 						i.num_comments,i.subreddit.display_name))
 
 		# if it's a comment we don't
 		elif postType == "comment":
-			# print('It\'s a comment')
 			continue
 		# if it's something else, I wanna know.
 		else:
@@ -118,15 +116,15 @@ def get_new_saves(me,old,conn,cursor,lim = 25):
 	print("{} posts committed to table 'saves'".format(len(toDatabase)))
 
 
-def check_post(r,me,conn,cursor):
+def check_post(r,conn,cursor):
 	# We get the current post
 	cursor.execute("SELECT * FROM current_post")
 	data = cursor.fetchall()
 
 	# if there's nothing in the DB we create a new post
 	if len(data)  == 0:
-		new_id = create_post(r,me,title)
-		edit_post(r,me,conn,cursor,new_id)
+		new_id = create_post(r,title)
+		edit_post(r,conn,cursor,new_id)
 		cursor.execute("""INSERT INTO current_post VALUES (?,?)""", [new_id,title])
 		conn.commit()
 		print('No post saved in DB. New post created. ID: {}'.format(new_id))
@@ -138,71 +136,62 @@ def check_post(r,me,conn,cursor):
 		sub = r.submission(id = post_id)
 		# if it's archived we create a new post
 		if sub.archived:
-			new_id = create_post(r,me,post_title)
-			edit_post(r,me,conn,cursor,new_id)
+			new_id = create_post(r,post_title)
+			edit_post(r,conn,cursor,new_id)
 			cursor.execute("""INSERT INTO current_post VALUES (?,?)""", [new_id,post_title])
 			conn.commit()
 			print("Post archived. New post created. ID: {}".format(new_id))
 		# otherwise we just edit the existing one
 		else:
-			edit_post(r,me,conn,cursor,post_id)
+			edit_post(r,conn,cursor,post_id)
 			print("Post edited.")
 
 
 # Actually, this might not be needed anyway. I can just keep a running tally
 # in the database, and just edit the submission from there
-def read_post(r,me,conn,cursor):
+def read_post(r,post_id):
 
-	cursor.execute("SELECT * FROM current_post")
-	data = cursor.fetchall()
-	post_id = data[0][0]
-	post_title = data[0][1]
+	# From when conn,cursor was also arguments
+	# cursor.execute("SELECT * FROM current_post")
+	# data = cursor.fetchall()
+	# post_id = data[0][0]
+	# post_title = data[0][1]
 
 	p = r.submission(id = post_id)
+	title = p.title
 	body = p.selftext
 	lines = body.split('\n')
-	ppp = pp.PrettyPrinter(width = 1000)
-	ppp.pprint(lines)
 	last_rows = []
 	first_rows = []
 	categories = []
 
-	# for num,text in enumerate(lines):
+	for num,text in enumerate(lines):
+		if len(text) != 0:
+			if text[0] == "#":
+				print("Found the title:")
+				print(text[1:])
+				print(lines[num+3])
+				first_rows.append(num+3)
+				categories.append(text[1:])
+		else:
+			print("Found the last rows:")
+			print(lines[num-1])
+			last_rows.append(num-1)
+
+	return title,body,lines,categories,first_rows,last_rows
+
+def populate_db(r,conn,cursor,post_id):
+	title,body,lines,categories,first_rows,last_rows = read_post(r,post_id)
 
 
-	# 	if len(text) != 0:
-	# 		if text[0] == "#":
-	# 			print("Found the title:")
-	# 			print(text[1:])
-	# 			print(lines[num+3])
-	# 			first_rows.append(num+3)
-	# 			categories.append(text[1:])
-	# 	else:
-	# 		print("Found the last rows:")
-	# 		print(lines[num-1])
-	# 		last_rows.append(num-1)
-
-
-
-	# This regex searches for a new line charactor, followed by 3 times (some amount of
-	# words and spaces and a vertical bar) and lastly two newline characters.
-	# re_last_row = re.compile(r"""(\n[A-Za-z0-9 '"]*\|[A-Za-z0-9 ]*\|[A-Za-z0-9 ]*)(\n)(\n)""")
-	# last_rows = re_last_row.findall(body)
-
-	# # This regex searches for the titles of the table.
-	# re_title = re.compile(r"""#([A-Za-z0-9 '"]*)\n""")
-	# titles = re_title.findall(body)
-
-	return body,lines,categories,first_rows,last_rows
-
-def create_post(r,me,title):
+def create_post(r,title):
 	# Just creates the new self post and returns the ID.
 	subreddit = r.subreddit(subname)
 	submission = subreddit.submit(title,selftext="hello")
 	return submission.id
 
 
-def edit_post(r,me,conn,cursor,post_id):
+def edit_post(r,conn,cursor,post_id):
 	# The purpose of this function is to read the table of saved posts in the
 	# database, and then create the post necessary.
 	# To do this, the saves need to be loaded from the DB, along with the
@@ -271,16 +260,10 @@ def edit_post(r,me,conn,cursor,post_id):
 	submission.edit(body)
 
 
-
-
-
-
-
-
 if __name__ == '__main__':
 	r,me = bot_login()
 	conn,cursor = init_DB()
 	old_ids = get_old_ids(conn,cursor)
 	get_new_saves(me,old_ids,conn,cursor,lim = None)
-	check_post(r,me,conn,cursor)
-	read_post(r,me,conn,cursor)
+	check_post(r,conn,cursor)
+	# populate_db(r,conn,cursor,"661mz0")
